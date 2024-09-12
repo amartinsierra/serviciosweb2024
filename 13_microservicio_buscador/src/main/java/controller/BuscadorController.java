cccac1b6218695326bd34716f94bc2b8759aec78
amartinsierra <amartinsierra@gmail.com>
2024-09-12 13:42:08 +0200

    12-9-2024 13:45

--- 13_microservicio_buscador/src/main/java/controller/BuscadorController.java
+++ 13_microservicio_buscador/src/main/java/controller/BuscadorController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.ResultadoDto;
@@ -33,6 +34,9 @@ public class BuscadorController {
 		System.out.println("!!!!!!alta de "+resultado.getUrl());
 		return String.valueOf(service.agregar(resultado));
 	}
-	
+	@GetMapping(value="buscar",produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResultadoDto buscarPorUrl(@RequestParam("url") String url) {
+		return service.buscarPorUrl(url);
+	}
 	
 }
